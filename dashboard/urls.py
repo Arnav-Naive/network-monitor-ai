222149d4a61ba74203a47dc06ef5dd27fda86d96
Student ApnaCollege <arnavfating09@gmail.com>
2026-05-06 22:25:32 +0530

    day 02: added Django dashboard to visualize monitoring data

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from monitor import views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('', views.dashboard_view, name='dashboard'),
 ]
--- monitor/views.py
+++ monitor/views.py
@@ -1,3 +1,17 @@
+import csv
+import os
 from django.shortcuts import render
 
-# Create your views here.
+def dashboard_view(request):
+    # Read CSV file
+    logs = []
+    csv_path = 'data/logs.csv'
+    
+    if os.path.exists(csv_path):
+        with open(csv_path, 'r') as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                logs.append(row)
+    
+    # Send data to HTML template
+    return render(request, 'monitor/dashboard.html', {'logs': logs})
